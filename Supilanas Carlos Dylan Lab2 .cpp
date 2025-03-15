a9d00350023baf2cb7a9d627893191e6dbdd3d97
CarlosDylanSupilanas <24-18471@g.batstate-u.edu.ph>
2025-03-15 11:18:38 +0800

    ACTIVITY 2 PART 2

--- Supilanas Carlos Dylan Lab2 .cpp
+++ Supilanas Carlos Dylan Lab2 .cpp
@@ -22,7 +22,7 @@ void traverse(Node *head){
     while(temp !=NULL){
         cout << temp->songName<< "->"<<endl;
         if(temp->link == NULL){
-            cout << "NULL"<<endl;
+            cout << "NULL" << endl; 
         }
 
         temp = temp->link;
@@ -78,19 +78,109 @@ string insertAfter(string after, string data, Node *head){
     temp->link = newNode;
 
     return "An new node has been added after " + after + "\n";
+
+}
+
+string deleteAtEnd(Node *head){
+    if(head == NULL){
+     return "The linked list is empty \n";
+    }
+
+    if(head->link == NULL){
+         delete head;
+         return "The head has been deleted \n";
+    }
+
+    Node *temp = new Node;
+    temp = head;
+
+    while(temp->link->link != NULL){
+        temp = temp->link;
+    }
+
+    temp->link = NULL;
+
+    return "A node has been deleted at the end \n";
+}
+
+Node *deleteFromBeginning(Node *head){
+    if(head == NULL){
+        cout << "the linked list is empty \n " << endl;
+        return NULL;
+    }
+
+    if(head->link == NULL){
+        delete head;
+    }
+    head = head->link;
+
+    cout <<"A node has been delete from the beginning \n" << endl;
+
+    return head;
+}
+
+Node *deleteFromGivenNode(string givenNode, Node *head){
+    if(head == NULL){
+        cout << "the linked list is empty. \n" << endl;
+        return NULL;
+    }
+
+    if(head->songName.compare(givenNode) == 0){
+        head = deleteFromBeginning(head);
+        cout << "The Node " + givenNode + "has been deleted. \n";
+        return head;
+    }
+
+    Node *temp = new Node;
+    Node *next = new Node;
+    temp = head; 
+    next = temp->link;
+
+    while(next->songName.compare(givenNode) != 0){
+        if(temp == NULL){
+            cout << "No such node exist. \n" << endl;
+            return head;
+        
+        }
+        next = next->link;
+        temp = temp->link;
+    }
+    
+    temp->link = next->link;
+    cout << "The Node " + givenNode + "has been deleted. \n" << endl;
+    return head;
 }
 
 
 
+int main (){
+   
+   Node *head = createNode("Sanctuary by joji");
+   
+   head = insertAtEnd("Cold as Ice by Blacklite District", head);
+   head = insertAtEnd("The Struggle by Blacklite District", head);
+   head = insertAtEnd("We are the Danger by Blacklite District", head);
+   head = insertAtBeginning("Spectre by Alan Walker", head);
+   head = insertAtBeginning("Faded by Alan Walker", head);
+   head = insertAtBeginning("We might Fall by Ghastly", head);
+   head = insertAtBeginning("Hall of Fame by The Script", head);
+   
+   
+   string after = insertAfter("Spectre by Alan Walker", "Simpleng tulad mo", head);
+   cout << after;
 
-int main(){
-    Node *head = createNode("Sanctuary by Joji");
-    traverse(head);
+   string after1 = insertAfter("Faded by Alan Walker", "Pusong bato", head);
+   cout << after1;
 
-    head = insertAtEnd("Sunday Morning by Maroon 5", head);
-    traverse(head);
-  
-    
+   string after2 = insertAfter("We might Fall by Ghastly", "tagulan", head);
+   cout << after2;
+   
+   deleteAtEnd(head);
+   deleteFromBeginning(head);
+   deleteFromGivenNode("We might Fall by Ghastly", head);
+ 
+ 
+   traverse(head);
 
 
     return 0;
